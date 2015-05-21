4cd675a0d6035dfa62fc3ae2d22ea3675ca6ce88
Alper <karabulutalper@gmail.com>
2015-05-21 16:52:23 +0300

    jQuery Event Handler

---
+++ csknz.src.js
@@ -0,0 +1,11 @@
+function initialize( ){
+	alert("document.ready");
+	//alert($("#logo").attr("class"));
+}
+
+$(document).ready(function(){
+	initialize();
+});
+function initialize2(){
+	alert("onload");
+}
